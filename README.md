e76fcb21c497e24b2203d9135bda468e8840a61c
Peter deHaan <peter@deseloper.com>
2016-04-14 17:16:12 -0700

    Will it deploy?

---
+++ README.md
@@ -0,0 +1,1 @@
+Basically this: [Integrating with Travis CI](https://surge.sh/help/integrating-with-travis-ci)
---
+++ index.js
@@ -0,0 +1,20 @@
+const writeFile = require("fs").writeFileSync;
+const join = require("path").join;
+
+const contents = `<!doctype html>
+<html>
+<body>
+<h1>${process.env.TRAVIS_REPO_SLUG}</h1>
+<strong>${process.env.TRAVIS_NODE_VERSION}</strong>
+<ul>
+<li>TRAVIS_BRANCH: ${process.env.TRAVIS_BRANCH}</li>
+<li>TRAVIS_BUILD_ID: ${process.env.TRAVIS_BUILD_ID}</li>
+<li>TRAVIS_BUILD_NUMBER: ${process.env.TRAVIS_BUILD_NUMBER}</li>
+<li>TRAVIS_COMMIT: ${process.env.TRAVIS_COMMIT}</li>
+<li>TRAVIS_PULL_REQUEST: ${process.env.TRAVIS_PULL_REQUEST}</li>
+<li>TRAVIS_TAG: ${process.env.TRAVIS_TAG}</li>
+</ul>
+</body>
+</html>`;
+
+writeFile(join('dist', 'index.html'), contents, 'utf-8');
